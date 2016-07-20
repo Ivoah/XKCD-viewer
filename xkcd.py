222d8f660a2f63b50033c39c6fa16e40ec89e714
Noah Rosamilia <ivoahivoah@gmail.com>
2016-07-20 15:46:02 -0400

    Add search and sharing. Also open explainxkcd.com

--- xkcd.py
+++ xkcd.py
@@ -4,8 +4,9 @@ import io
 import dialogs
 import random
 import console
-import os.path
+import os
 import json
+import clipboard
 
 def get_info(num):
     fp = 'cache/{}.json'.format(num)
@@ -41,7 +42,6 @@ def get_comic(url):
             return ui.Image.from_data(req.content)
 
 def load_comic(view, num):
-    view['gear'].start()
     console.show_activity()
     comic = get_info(num)
     if comic or num == 404:
@@ -54,12 +54,27 @@ def load_comic(view, num):
         
         view['comic'].image = get_comic(view.comic['img'])
         view['title'].text = view.comic['title']
+        view.name = view.comic['title']
     
-    view['gear'].stop()
     console.hide_activity()
 
 @ui.in_background
-def prev(sender): 
+def search(sender):
+    sender.superview['tbl_search'].data_source.items = []
+    for file in os.listdir('cache'):
+        if file.split('.')[-1] != 'json': continue
+        with open('cache/{}'.format(file)) as f:
+            comic = json.load(f)
+            search_text = sender.text.lower()
+            if search_text in comic['title'].lower() or search_text in comic['transcript'].lower() or search_text in comic['alt'].lower():
+                sender.superview['tbl_search'].data_source.items.append('{num}: {title}'.format(**comic))
+
+def select_comic(sender):
+    load_comic(main_view, int(sender.items[sender.selected_row].split(':')[0]))
+    nav.pop_view()
+
+@ui.in_background
+def prev(sender):
     load_comic(sender.superview, sender.superview.current - 1)
 
 @ui.in_background
@@ -72,7 +87,17 @@ def slider_changed(sender):
 
 @ui.in_background
 def alt(sender):
-    dialogs.alert('{month}/{day}/{year}'.format(**sender.superview.comic), sender.superview.comic['alt'])
+    ans = console.alert('{month}/{day}/{year}'.format(**sender.superview.comic), sender.superview.comic['alt'], 'Copy link', 'Explain')
+    if ans == 1:
+        clipboard.set('http://xkcd.com/{}/'.format(sender.superview.current))
+        console.hud_alert('Copied!')
+    elif ans == 2:
+        explain_view.load_url('http://www.explainxkcd.com/wiki/index.php/{}'.format(sender.superview.current))
+        nav.push_view(explain_view)
+
+@ui.in_background
+def share(sender):
+    dialogs.share_image(sender.superview['comic'].image)
 
 @ui.in_background
 def rand(sender):
@@ -86,8 +111,22 @@ except requests.exceptions.ConnectionError:
     with open('cache/latest') as f:
         latest = int(f.read())
 
-v = ui.load_view()
-v.current = latest
-v.present()
-load_comic(v, v.current)
 
+main_view = ui.load_view()
+main_view.current = latest
+load_comic(main_view, main_view.current)
+
+nav = ui.NavigationView(main_view)
+
+search_view = ui.load_view('search')
+search_view['tbl_search'].data_source.items = []
+search_view['tbl_search'].data_source.action = select_comic
+
+explain_view = ui.WebView()
+explain_view.name = 'Explain'
+explain_view.right_button_items = [
+    ui.ButtonItem(None, ui.Image('iob:chevron_right_32'), lambda _: explain_view.go_forward()),
+    ui.ButtonItem(None, ui.Image('iob:chevron_left_32'), lambda _: explain_view.go_back())
+]
+
+nav.present()
