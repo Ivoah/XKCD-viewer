1194bf6a463d10ffe53b2c62898be896eb23b29c
Noah Rosamilia <ivoahivoah@gmail.com>
2016-10-03 15:29:16 -0400

    Make XKCD-Viewer play nicely with comic 404

--- xkcd.py
+++ xkcd.py
@@ -9,6 +9,7 @@ import json
 import clipboard
 import glob
 import PieView
+import objc_util
 
 def get_info(num):
     fp = '{}.json'.format(num)
@@ -43,20 +44,27 @@ def get_comic(url):
                 f.write(req.content)
             return ui.Image.from_data(req.content)
 
+@ui.in_background
 def load_comic(view, num):
+    if num == 404:
+        view['comic'].image = ui.Image('../loading.png')
+        view.current = num
+        view['comic_num'].text = str(num)
+        view['slider'].value = num/latest
+        view.comic = None
+        objc_util.ObjCInstance(view.navigation_view).navigationController().topViewController().title = objc_util.ns('404 - Not Found')
+        return 
     console.show_activity()
     comic = get_info(num)
-    if comic or num == 404:
+    if comic:
         view['comic'].image = ui.Image('../loading.png')
-        view['title'].text = 'Loading comic...'
         view.current = num
         view['comic_num'].text = str(num)
         view['slider'].value = num/latest
         view.comic = comic
         
         view['comic'].image = get_comic(view.comic['img'])
-        view['title'].text = view.comic['title']
-        view.name = view.comic['title']
+        objc_util.ObjCInstance(view.navigation_view).navigationController().topViewController().title = objc_util.ns(view.comic['title'])
     
     console.hide_activity()
 
@@ -69,15 +77,17 @@ def search(sender):
         sender.superview['pie'].set_value(i/num_files)
         with open(file) as f:
             comic = json.load(f)
+            found = True
             for word in sender.text.lower().split():
-                if word in ' '.join([str(v) for v in comic.values()]).lower():
-                    sender.superview['tbl_search'].data_source.items.append('{num}: {title}'.format(**comic))
-                    break
+                if word not in ' '.join([str(v) for v in comic.values()]).lower():
+                    found = False
+            if found: sender.superview['tbl_search'].data_source.items.append('{num}: {title}'.format(**comic))
     sender.superview['pie'].set_value(0)
 
+@ui.in_background
 def select_comic(sender):
-    load_comic(main_view, int(sender.items[sender.selected_row].split(':')[0]))
     nav.pop_view()
+    load_comic(main_view, int(sender.items[sender.selected_row].split(':')[0]))
 
 @ui.in_background
 def prev(sender):
@@ -93,6 +103,7 @@ def slider_changed(sender):
 
 @ui.in_background
 def alt(sender):
+    if not sender.superview.comic: return
     ans = console.alert('{month}/{day}/{year}'.format(**sender.superview.comic), sender.superview.comic['alt'], 'Copy link', 'Explain')
     if ans == 1:
         #dialogs.share_url('http://xkcd.com/{}/'.format(sender.superview.current))
@@ -133,7 +144,6 @@ main_view.left_button_items = [
 main_view.right_button_items = [
     ui.ButtonItem(None, ui.Image('iob:share_32'), share)
 ]
-load_comic(main_view, main_view.current)
 
 nav = ui.NavigationView(main_view)
 
@@ -152,3 +162,4 @@ explain_view.right_button_items = [
 ]
 
 nav.present(hide_title_bar = False)
+load_comic(main_view, main_view.current)
